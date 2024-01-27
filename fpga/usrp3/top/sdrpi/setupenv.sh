b74d03438e076df0e641470dc0939432bd048441
Marcel Graber <marcel@clever.design>
2024-01-27 11:45:56 +0100

    removing unused device ESP310-sg3

--- fpga/usrp3/top/sdrpi/setupenv.sh
+++ fpga/usrp3/top/sdrpi/setupenv.sh
@@ -2,15 +2,14 @@
 
 VIVADO_VER=2021.1
 VIVADO_VER_FULL=2021.1_AR76780
-DISPLAY_NAME="USRP-E31x"
+DISPLAY_NAME="USRP-SDRPI"
 REPO_BASE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 
 declare -A PRODUCT_ID_MAP
-PRODUCT_ID_MAP["E310_SG1"]="zynq/xc7z020/clg484/-1"
-PRODUCT_ID_MAP["E310_SG3"]="zynq/xc7z020/clg484/-3"
+PRODUCT_ID_MAP["E310_SG1"]="zynq/xc7z020/clg400/-1"
 
 # Set default part for simulation
 export ARCH=zynq
-export PART_ID="xc7z020/clg484/-3"
+export PART_ID="xc7z020/clg400/-1"
 
 source $REPO_BASE_PATH/tools/scripts/setupenv_base.sh
